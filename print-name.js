51c7dbbad38e7a6e2571141f6ddcfa750ef2bc7e
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-08-19 04:53:28 +0100

    add program to print my name

---
+++ print-name.js
@@ -0,0 +1,9 @@
+// print my name
+
+//checking if navigator is a browser or nodejs enviroment
+if (typeof window === "object") {
+    //show a popup
+    window.alert("Ngwa Sedrick Meh")
+} else {
+    console.log("Ngwa Sedrick Meh")
+}
